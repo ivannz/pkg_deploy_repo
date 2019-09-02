b4b2b7431382de54baa407ce4d82cc541ee9666e
Ivan <ivannz@yandex.ru>
2019-09-02 13:02:04 +0300

    added bar skeleton

---
+++ tutorials/bar-tutorial/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+
+setup(
+    name="bar",
+    version="0.1",
+    packages=[
+        "bar",
+        "bar.sub",
+    ],
+    install_requires=[
+        "numpy",
+        "tqdm",
+        "matplotlib",
+        "torch",
+        "torchvision",
+    ],
+    package_data={
+        "schedule": ["data/sched daily.txt",]
+    }
+)
