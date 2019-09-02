d2f36bbf082ef793bfea06cc4c7ff796e9ede6f8
Ivan <ivannz@yandex.ru>
2019-09-02 13:12:30 +0300

    fixed import

--- tutorials/bar-tutorial/bar/__init__.py
+++ tutorials/bar-tutorial/bar/__init__.py
@@ -1,5 +1,5 @@
 from pkg_resources import resource_stream
 
 def get_schedule():
-    with pkg_resources.resource_stream("bar", "schedule") as fin:
+    with resource_stream("bar", "schedule") as fin:
         return fin.readlines()
